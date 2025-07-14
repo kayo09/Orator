c4735164f507e9083aac164aa0681811a758a7e3
Kay Parmar <68217041+kayo09@users.noreply.github.com>
2025-07-14 00:59:13 -0400

    Update app.py

--- Frontend_Streamlit/app.py
+++ Frontend_Streamlit/app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 )
 
 # Constants
-API_BASE_URL = os.getenv("API_BASE_URL", "https://orator-production.up.railway.app")
+API_BASE_URL = os.getenv("API_BASE_URL", "https://orator-production.up.railway.app:8000")
 
 class APIRoutes:
     @staticmethod
